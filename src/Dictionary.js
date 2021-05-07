a7eee82f67290bad37b5d9b73609f7f708127faa
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2021-05-07 14:57:16 +0200

    documentation link added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,6 +14,7 @@ export default function Dictionary() {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    // documentation https://dictionaryapi.dev/
   }
   return (
     <div className="Dictionary">
