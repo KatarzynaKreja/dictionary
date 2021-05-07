743f0c183eae5f0b97b91bd55f7e8e0f6f1db8fd
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2021-05-07 14:56:07 +0200

    dictionary API immigration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import React from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
