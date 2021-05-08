e598a30051b6d932c8221f1eea5f2e0ef8a58ef3
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2021-05-08 15:15:21 +0200

    synonyms displayed

---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <h6>Synonyms</h6>
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}> {synonym}</span>;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
