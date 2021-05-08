9e9cbb5152de96d97648929fef0b3e5b695bc605
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2021-05-08 13:32:53 +0200

    meaning display added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import React from "react";
 import axios from "axios";
+import Result from "./Result";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState(null);
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
   function handleResponse(response) {
-    console.log(response.data);
+    setResult(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -21,6 +24,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeyword} />
       </form>
+      <Result result={result} />
     </div>
   );
 }
